c6e0e36e46dd3ee5cfdc17a704e795240fb80f03
Mmah-Zombo <www.mariamzombo@gmail.com>
2024-09-10 01:06:18 +0000

    changed the dark mode border color for the input, select and textarea

--- resources/views/components/listing-form/input.blade.php
+++ resources/views/components/listing-form/input.blade.php
@@ -3,5 +3,5 @@
 <input {{ $attributes->merge(['type' => $type, 'id' => $name, 'name'=> $name ]) }} value="{{old($name)}}"
 class="peer pe-0 ps-2 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200
 focus:border-t-transparent focus:border-x-transparent focus:border-b-custom-blue focus:ring-0 disabled:opacity-50
-disabled:pointer-events-none dark:border-b-neutral-700 dark:text-white dark:placeholder-neutral-500"
+disabled:pointer-events-none dark:border-b-neutral-600 dark:text-white dark:placeholder-neutral-500"
 required>
--- resources/views/components/listing-form/textarea.blade.php
+++ resources/views/components/listing-form/textarea.blade.php
@@ -1,6 +1,6 @@
 @props(['name'])
 
 <textarea {{ $attributes->merge(['id' => $name, 'name'=> $name ]) }} value="{{old($name)}}"
-    class="peer pe-0 ps-2 block w-full bg-transparent border-y-2 border-x-transparent border-y-gray-200 focus:border-x-transparent focus:border-y-custom-blue focus:ring-0 disabled:opacity-50 disabled:pointer-events-none dark:border-y-neutral-700 dark:text-white dark:placeholder-neutral-500"
+    class="peer pe-0 ps-2 block w-full bg-transparent border-y-2 border-x-transparent border-y-gray-200 focus:border-x-transparent focus:border-y-custom-blue focus:ring-0 disabled:opacity-50 disabled:pointer-events-none dark:border-y-neutral-600 dark:text-white dark:placeholder-neutral-500"
     required>
 </textarea>
