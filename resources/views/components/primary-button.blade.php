3743b229067d265a4c2c33a7f03c35465ec3d3a4
Mmah-Zombo <www.mariamzombo@gmail.com>
2024-09-10 23:02:56 +0000

    Made the filter bar responsive

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,4 @@
-<button class="bg-custom-blue hover:bg-[#0082CC] text-white font-bold py-2 px-5 rounded-md flex items-center">
+<button
+    class="bg-custom-blue hover:bg-[#0082CC] text-white font-bold py-2 px-5 rounded-md flex items-center justify-center">
     {{ $slot }}
 </button>
--- resources/views/livewire/filter-bar.blade.php
+++ resources/views/livewire/filter-bar.blade.php
@@ -1,23 +1,19 @@
 <div
   class="sticky top-16 w-full bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800 shadow-md p-4 z-40">
-  <div class="flex flex-wrap items-center gap-4">
-    <div class="flex-grow">
+  <div class="grid grid-cols-2 md:flex flex-wrap items-center gap-4">
+    <div class="col-span-2 w-full md:w-auto flex-grow">
       <input
         class="w-full px-3 py-2 dark:bg-gray-800 dark:border-none border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-custom-purple focus:border-transparent"
         placeholder="Location..." type="search" />
     </div>
     <div class="relative">
-      <x-filter-select name="category">
+      <x-filter-select name="category" class="w-full">
         <option value="all">All</option>
         <option value="sale" selected>For Sale</option>
         <option value="rent">For Rent</option>
         <option value="sublet">For Subletting</option>
       </x-filter-select>
-      {{-- <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-        </svg>
-      </div> --}}
+
     </div>
     <div class="relative">
       <x-filter-select name="price">
@@ -27,11 +23,7 @@
         <option value="50-100">$50 - $100</option>
         <option value="100+">$100+</option>
       </x-filter-select>
-      {{-- <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-        </svg>
-      </div> --}}
+
     </div>
     <div class="relative">
       <x-filter-select name="rooms">
@@ -41,11 +33,7 @@
         <option value="3+">3+ Stars</option>
         <option value="2+">2+ Stars</option>
       </x-filter-select>
-      {{-- <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-        </svg>
-      </div> --}}
+
     </div>
     <div class="relative">
       <x-filter-select name="baths">
@@ -55,14 +43,10 @@
         <option value="europe">Europe</option>
         <option value="asia">Asia</option>
       </x-filter-select>
-      {{-- <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-        </svg>
-      </div> --}}
+
     </div>
-    <x-primary-button>
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24"
+    <x-primary-button class="text-center">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 -ms-2" fill="none" viewBox="0 0 24 24"
         stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
