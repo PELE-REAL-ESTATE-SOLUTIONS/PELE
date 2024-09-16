ecf6ca8e57ce1c0a1607302e99e6d5ff39f9301a
Mmah-Zombo <www.mariamzombo@gmail.com>
2024-09-16 22:11:53 +0000

    Made the property detail page responsive

--- resources/views/property-listing/show.blade.php
+++ resources/views/property-listing/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 
     <div class="py-5">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="mb-4 w-full flex items-center justify-between">
                 <div>
                     <h1 class="text-xl font-bold text-custom-blue leading-tight">Listing Details</h1>
@@ -10,15 +10,15 @@
                 </div>
             </div>
 
-            <div class="relative overflow-hidden h-max grid grid-cols-4 gap-4 mb-4">
+            <div class="relative overflow-hidden h-fit md:h-max md:grid grid-cols-4 gap-4 mb-4">
                 @foreach($firstFivePictures as $picture)
                 @if ($loop->first)
-                <div class="h-[30rem] col-span-2 relative">
+                <div class="md:h-[30rem] col-span-2 relative">
                     <img src="{{ asset('storage/' . $picture) }}" alt="Property Picture" class="h-full w-full" />
                 </div>
                 @endif
                 @endforeach
-                <div class="h-[30rem] col-span-2 grid grid-cols-2 grid-rows-2 gap-4">
+                <div class="hidden h-[30rem] col-span-2 md:grid grid-cols-2 grid-rows-2 gap-4">
                     @foreach($firstFivePictures as $picture)
                     @if (!$loop->first)
                     <div>
@@ -38,7 +38,7 @@
 
             <div class="bg-transparent mb-8">
                 <div class=" mb-4">
-                    <div class="flex items-start justify-between">
+                    <div class="md:flex items-start justify-between mb-6 md:mb-0">
                         <h1 class="text-3xl font-bold mb-2 dark:text-white">NLE {{ number_format($property->price) }} -
                             {{ strtoupper($property->property_type) }}</h1>
 
@@ -47,28 +47,29 @@
                             {{
                             strtoupper($property->listing_type) }}</span>
                     </div>
-                    <div class="flex justify-between items-center w-full">
+                    <div class="md:flex justify-between items-center w-full">
                         <p class="text-xl text-gray-600 dark:text-gray-400">{{ strtoupper($property->location) }}
                         </p>
-                        <div class="flex space-x-4 text-lg dark:text-gray-400">
+                        <div class="md:flex space-x-4 text-lg dark:text-gray-400">
                             <span><strong>{{ $property->bedrooms }}</strong> bedrooms |</span>
                             <span><strong>{{ $property->bathrooms }}</strong> bathrooms |</span>
                             <span><strong>{{ $property->area }}</strong> sqft</span>
                         </div>
                     </div>
                 </div>
-                <div class="flex space-x-4 mb-8">
-                    <span class="bg-custom-blue/10 text-custom-blue px-4 py-2 rounded-md text-sm">{{
+                <div class="flex flex-wrap md:space-x-4 mb-8">
+                    <span class="bg-custom-blue/10 text-custom-blue px-4 py-2 rounded-md text-sm mr-4 md:mr-0">{{
                         $property->livingrooms }}
                         Living Room</span>
-                    <span class="bg-custom-blue/10 text-custom-blue px-4 py-2 rounded-md text-sm">{{ $property->kitchens
+                    <span class="bg-custom-blue/10 text-custom-blue px-4 py-2 rounded-md text-sm mr-4 md:mr-0">{{
+                        $property->kitchens
                         }}
                         Kitchens</span>
-                    <span class="bg-custom-blue/10 text-custom-blue px-4 py-2 rounded-md text-sm">{{
+                    <span class="bg-custom-blue/10 text-custom-blue px-4 py-2 rounded-md text-sm mt-4 md:mt-0">{{
                         $property->diningrooms }}
                         Dining Room</span>
                 </div>
-                <div class="grid grid-cols-3 gap-8">
+                <div class="flex flex-col-reverse md:grid grid-cols-3 gap-8">
                     <div class="col-span-2 border border-custom-blue rounded-lg p-6">
                         <h3 class="text-xl font-semibold mb-4 dark:text-white">Description</h3>
                         <p class="text-gray-600 mb-4 dark:text-gray-300">
